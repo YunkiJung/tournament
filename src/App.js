24f19ec8698481dc0e42b64256a59865817b3286
YunkiJung <100543183+YunkiJung@users.noreply.github.com>
2022-11-27 23:02:30 -0500

    routing added, libraries added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Navbar from './layout/Navbar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path='/' element={<div>home page</div>}></Route>
+        <Route path='/login' element={<div>login page</div>}></Route>
+        <Route path='/register' element={<div>register page</div>}></Route>
+        <Route path='/activelist' element={<div>active tournaments page</div>}></Route>
+        <Route path='/pastlist' element={<div>past tournaments page</div>}></Route>
+        <Route path='/create' element={<div>create new tournament page</div>}></Route>
+      </Routes>
+    </>
   );
 }
 
